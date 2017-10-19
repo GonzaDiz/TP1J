92194f6a37e9683f07a71237acb2a1186c1e59cd
GonzaDiz <dizmartin.gonzalo@gmail.com>
2017-10-19 12:48:25 -0300

    Clases Base Creadas Con metodos base

--- TP1J/src/src/AlgoBay.java
+++ TP1J/src/src/AlgoBay.java
@@ -1,5 +1,62 @@
+package src;
 
 
 public class AlgoBay {
+
+	public Object getCantidadDeProductos() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Producto agregarProductoConPrecio(String string, int i) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Producto getProducto(String string) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void agregarProductoEnCompra(Producto productoZapatillaNike, Compra compraConEnvioYGarantia) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public Compra crearNuevaCompra() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public int getPrecioTotalDe(Compra compra) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public Compra crearNuevaCompraConEnvio() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Compra crearNuevaCompraConGarantia() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Compra crearNuevaCompraConEnvioYGarantia() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Cupon crearCuponConPorcentaje(int i) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void agregarCuponEnCompra(Cupon cupon20Porciento, Compra compraConEnvio) {
+		// TODO Auto-generated method stub
+		
+	}
+
 
 }
