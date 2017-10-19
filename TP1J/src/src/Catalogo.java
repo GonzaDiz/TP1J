92d9da64902a33c3e67ac0435fb3987b0b30e57f
Gonza <Gonza@PC-GONZA>
2017-10-19 16:15:20 -0300

    Nada

---
+++ TP1J/src/src/Catalogo.java
@@ -0,0 +1,32 @@
+package src;
+
+import java.util.ArrayList;
+
+public class Catalogo {
+	
+	private ArrayList<Producto> productos;
+	
+	public Catalogo() {
+		this.productos = new ArrayList<Producto>();
+	}
+
+	public Producto agregarProductoConPrecio(String nombre,int precio) {
+		Producto producto = new Producto(nombre,precio);
+		productos.add(producto);
+		return producto;
+	}
+
+	public int getCantidadDeProductos() {
+		return productos.size();
+	}
+
+	public Producto getProducto(String nombre) {
+		for (Producto p : productos) {
+			if(nombre.equals(p.getNombre())) {
+				return p;
+			}
+		}
+		return null;
+	}
+
+}
